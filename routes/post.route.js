233fe5fa8418bada8204aab239aad197005016cb
Olaniyan Miracle <115111556+Tscript01@users.noreply.github.com>
2025-12-02 12:08:37 +0100

    Update post.route.js

--- routes/post.route.js
+++ routes/post.route.js
@@ -18,25 +18,25 @@ const upload = multer({
   limits: {
     fileSize: 10 * 1024 * 1024,
   },
-  // fileFilter: (req, file, cb) => {
-  //   const allowedTypes = [
-  //     "image/jpeg",
-  //     "image/png",
-  //     "image/gif",
-  //     "image/webp",
-  //   //   "application/pdf",
-  //   //   "application/msword",
-  //   //   "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-  //   //   "text/plain",
-  //   //   "application/zip",
-  //   ];
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = [
+      "image/jpeg",
+      "image/png",
+      "image/gif",
+      "image/webp",
+    //   "application/pdf",
+    //   "application/msword",
+    //   "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    //   "text/plain",
+    //   "application/zip",
+    ];
 
-  //   if (allowedTypes.includes(file.mimetype)) {
-  //     cb(null, true);
-  //   } else {
-  //     cb(new Error("File type not allowed"), false);
-  //   }
-  // },
+    if (allowedTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("File type not allowed"), false);
+    }
+  },
 });
 
 postRouter.get('/', getPosts);
